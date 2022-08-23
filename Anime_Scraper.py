0c3e62ea2986e55fd3213a138041f80d6d6c3071
EJKonge <ejkonge@gmail.com>
2022-08-23 21:04:32 +0100

    Creates scraper class

---
+++ Anime_Scraper.py
@@ -0,0 +1,43 @@
+import selenium
+from selenium import webdriver 
+import requests 
+from bs4 import BeautifulSoup
+import pandas as pd
+from selenium.webdriver.common.by import By
+import time
+
+class Anime_Scraper:
+
+    def __init__(self, url : str = "https://www.imdb.com/search/keyword/?keywords=anime&ref_=kw_nxt&mode=detail&page=1&sort=moviemeter,asc"):
+
+        self.driver = webdriver.Chrome()
+        self.driver.get(url)
+
+    def get_titles(self):
+        title = []
+        link = []
+        year = []
+
+        block = self.driver.find_element(By.CLASS_NAME, 'lister-item')
+        for i in range(0,50):
+            ftitle = block[i].find_element(By.CLASS_NAME, 'lister-item-header').text
+            forder = block[i].find_element(By.CLASS_NAME, 'lister-item-index').text
+            fyear = ftitle[-6:]
+            ftitle = ftitle.replace(forder+' ', '')[:-7 ]
+            flink = block[i].find_element_by_link_text(ftitle).get_attribute('href')
+            title.append(ftitle)
+            year.append(fyear)
+            link.append(flink)
+
+    def run_scraper(self):
+            self.get_titles()
+
+            
+
+    def quit_scraper(self):
+        self.driver.quit()
+                
+if __name__ == '__main__':
+    Anime = Anime_Scraper()
+    Anime.run_scraper()
+    Anime.quit_scraper()
