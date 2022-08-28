f1bde473f032289ac8d8ec0973422d8764c3dd91
EJKonge <ejkonge@gmail.com>
2022-08-28 01:12:33 +0100

    Webscraper first data collection and local exports

--- test.py
+++ test.py
@@ -3,62 +3,109 @@ from selenium import webdriver
 import requests 
 from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
 from selenium.webdriver.common.by import By
 import time
+import uuid
 
 class Anime_Scraper:
 
-    def __init__(self, url : str = "https://www.imdb.com/search/keyword/?keywords=anime&ref_=kw_nxt&mode=detail&page=1&sort=moviemeter,asc"):
+    def __init__(self,  url : str = "https://www.imdb.com/search/keyword/?keywords=anime&ref_=kw_nxt&mode=detail&page=1&sort=moviemeter,asc"):
 
         self.driver = webdriver.Chrome()
         self.driver.get(url)
+        self.title= []
+        self.year= []
+        self.link= []
+        self.genre= []
+        self.rating= []
+        self.id= []
+        self.uuid= []
+        self.page = 1
 
     def get_titles(self):
-        time.sleep(2)
-        i=50
-        page = 1
-        block = self.driver.find_element(By.XPATH, f'//*[@id="main"]/div/div[2]/div[3]/div[{i}]')
-        title = block.find_element(By.XPATH, f'//*[@id="main"]/div/div[2]/div[3]/div[{i}]/div[2]/h3/a').text
-        year = block.find_element(By.XPATH, f'//*[@id="main"]/div/div[2]/div[3]/div[{i}]/div[2]/h3/span[2]').text
-        link = block.find_element(By.XPATH, f'//*[@id="main"]/div/div[2]/div[3]/div[{i}]/div[2]/h3/a').get_attribute('href')
-        genre = block.find_element(By.XPATH, f'//*[@id="main"]/div/div[2]/div[3]/div[{i}]/div[2]/p[1]/span[5]').text
-        rating = block.find_element(By.XPATH, f'//*[@id="main"]/div/div[2]/div[3]/div[{i}]/div[2]/div/div[1]/strong').text
-        certificate = block.find_element(By.XPATH, f'//*[@id="main"]/div/div[2]/div[3]/div[{i}]/div[2]/p[1]/span[1]').text
-        runtime = block.find_element(By.XPATH, f'//*[@id="main"]/div/div[2]/div[3]/div[{i}]/div[2]/p[1]/span[3]').text
-        print(title)
-        print(year)
-        print(link + "\n" + genre + "\n" + rating + "\n" + certificate + "\n" + runtime)
-        time.sleep(2)
-        if page == 1:
-            self.first_page()
-            page +=1
-        else: 
-            self.next_page
-            page +=1
+        for i in range(1,51):
+            self.uuid.append(str(uuid.uuid4()))
+            block = self.driver.find_element(By.XPATH, f'//*[@id="main"]/div/div[2]/div[3]/div[{i}]')
+            try:
+                
+                try:
+                    title_temp = block.find_element(By.XPATH, f'//*[@id="main"]/div/div[2]/div[3]/div[{i}]/div[2]/h3/a').text
+                    self.title.append(title_temp)
+                except:
+                    title_temp = np.NAN
+                    self.title.append(title_temp)
+
+                try:
+                    year_temp = block.find_element(By.XPATH, f'//*[@id="main"]/div/div[2]/div[3]/div[{i}]/div[2]/h3/span[2]').text
+                    self.year.append(year_temp)
+                except:
+                    year_temp = np.NAN
+                    self.year.append(year_temp)
+
+                try:    
+                    link_temp = block.find_element(By.XPATH, f'//*[@id="main"]/div/div[2]/div[3]/div[{i}]/div[2]/h3/a').get_attribute('href')
+                    self.link.append(link_temp)
+                except:
+                    link_temp = np.NAN
+                    self.link.append(link_temp)
+
+                try:
+                    genre_temp = block.find_element(By.CLASS_NAME, 'genre').text
+                    self.genre.append(genre_temp)
+                except:
+                    genre_temp = np.NAN
+                    self.genre.append(genre_temp)
+
+                try:
+                    rating_temp = block.find_element(By.XPATH, f'//*[@id="main"]/div/div[2]/div[3]/div[{i}]/div[2]/div/div[1]/strong').text
+                    self.rating.append(rating_temp)
+                except:
+                    rating_temp = np.NAN
+                    self.rating.append(rating_temp)
+                
+                try:
+                    id_temp = link_temp[29:36]
+                    self.id.append(id_temp)
+                except:
+                    id_temp = np.NAN
+                    self.id.append(id_temp)
+
+
+            except:
+                continue
+    
+    def create_df(self):
+        anime_df = pd.DataFrame({'Title':self.title, 'Year':self.year, 'Link':self.link, 'Genres':self.genre, 'Rating':self.rating, 'ID':self.id, 'UUID':self.uuid})
+        anime_df.index +=1
+        print(anime_df)
+        anime_df.to_json(r'raw_data\data.json')
+        anime_df.to_csv('raw_data\data.csv')
 
     def run_scraper(self):
             self.get_titles()
-    
-    def first_page(self):
-        self.nextpage = self.driver.find_element(By.XPATH, '//*[@id="main"]/div/div[2]/div[1]/div/div[2]/a')
-        self.nextpage.click()
 
     def next_page(self):
-        self.nextpage = self.driver.find_element(By.XPATH, '//*[@id="main"]/div/div[2]/div[1]/div/div[2]/a[2]')
-        self.nextpage.click()
+        
+        if self.page == 1:
+            self.nextpage = self.driver.find_element(By.XPATH, '//*[@id="main"]/div/div[2]/div[1]/div/div[2]/a')
+            self.nextpage.click()
+            self.page +=1
 
+        else:
+            self.nextpage = self.driver.find_element(By.XPATH, '//*[@id="main"]/div/div[2]/div[1]/div/div[2]/a[2]')
+            self.nextpage.click()
+            self.page +=1
               
     def quit_scraper(self):
         self.driver.quit()
                 
 if __name__ == '__main__':
     Anime = Anime_Scraper()
-    time.sleep(5)
-    Anime.first_page()
-    time.sleep(5)
-    Anime.next_page()
+    Anime.run_scraper()
     time.sleep(5)
     Anime.next_page()
     time.sleep(5)
+    Anime.run_scraper()
+    Anime.create_df() 
     Anime.quit_scraper()
-    
