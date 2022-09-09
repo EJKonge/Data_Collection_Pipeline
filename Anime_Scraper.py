c0104fc9bd804d8834a5e4b42d2e44ef63b39f94
EJKonge <ejkonge@gmail.com>
2022-09-09 20:21:28 +0100

    adds options for webdriver

--- Anime_Scraper.py
+++ Anime_Scraper.py
@@ -30,9 +30,11 @@ class Anime_Scraper:
         """
         options = Options()
         options.add_argument("--headless")
-        options.add_argument("window-size=1920,1080")
+        options.add_argument("--window-size=1920,1080")
         options.add_argument('--disable-gpu')
-        self.driver = webdriver.Chrome(chrome_options=options)
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        self.driver = webdriver.Chrome(options=options)
         self.driver.get(url)
         self.title= []
         self.year= []
